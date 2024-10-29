85c352eb42ca7452411e191c0bf9020a97844b60
Jordan Youssef <jordanyoussef28@gmail.com>
2024-10-29 17:08:05 -0500

    fixed bugs

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-from views import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_all_locations, get_single_location, create_location, get_all_employees, get_single_employee, create_employee, get_all_customers, get_single_customer, create_customer
+from views import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_all_locations, get_single_location, create_location, delete_location, get_all_employees, get_single_employee, create_employee, delete_employee, get_all_customers, get_single_customer, create_customer, delete_customer
 
 
 # Here's a class. It inherits from another class.
@@ -163,6 +163,15 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Delete a single animal from the list
         if resource == "animals":
             delete_animal(id)
+            
+        if resource == "locations":
+            delete_location(id)
+            
+        if resource == "employees":
+            delete_employee(id)
+            
+        if resource == "customers":
+            delete_customer(id)
 
     # Encode the new animal and send in response
         self.wfile.write("".encode())
